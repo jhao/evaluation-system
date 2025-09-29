c73d84092ae05e8a24c585aff9d9004047336fdf
Hao Jin <haojinhj@gmail.com>
2025-09-29 19:09:29 +0800

    Update main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -105,4 +105,4 @@ def serve(path):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5001, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5001, debug=True,  allow_unsafe_werkzeug=True)
